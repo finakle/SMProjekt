09b8010159c632674613c4513bc059597010b79a
dariapiersa <dariapiersa17@gmail.com>
2022-01-06 17:32:32 +0100

    dodanie pola wyszukiwania i listy krajów

--- app/src/main/java/com/example/sm_projekt/MainActivity.java
+++ app/src/main/java/com/example/sm_projekt/MainActivity.java
@@ -115,6 +115,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goTrackCountries(View view) {
-
+        startActivity(new Intent(getApplicationContext(),AffectedCountries.class));
     }
 }
