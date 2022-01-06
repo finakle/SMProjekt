331cdf8df88e4900af269e3d796f7d4ce807c9c4
dariapiersa <dariapiersa17@gmail.com>
2022-01-06 18:21:47 +0100

    dodanie modeli klas, widok poszczegolnych krajow, Adapter cz.1

---
+++ app/src/main/java/com/example/sm_projekt/MyCustomAdapter.java
@@ -0,0 +1,47 @@
+package com.example.sm_projekt;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MyCustomAdapter extends ArrayAdapter<CountryModel> {
+
+    private Context context;
+    private List<CountryModel> countryModelsList;
+    private List<CountryModel> countryModelsListFiltered;
+
+    public MyCustomAdapter(Context context, List<CountryModel> countryModelsList) {
+        super(context, R.layout.list_custom_item, countryModelsList);
+
+        this.context = context;
+        this.countryModelsList = countryModelsList;
+        this.countryModelsListFiltered = countryModelsList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_custom_item,null,true);
+        TextView tvCountryName = view.findViewById(R.id.tvCountryName);
+        ImageView imageView = view.findViewById(R.id.imageFlag);
+
+        tvCountryName.setText(countryModelsListFiltered.get(position).getCountry());
+        Glide.with(context).load(countryModelsListFiltered.get(position).getFlag()).into(imageView);
+
+        return view;
+    }
+}
+
+
